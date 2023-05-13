3759aab12900e2c4e3696ecf16511458afaf8a87
quontral <quontralcom@gmail.com>
2023-05-13 05:40:39 +0300

    s

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -11,6 +11,10 @@ import Icon_profile from '../icons/profile';
 import Icon_settings from '../icons/settings';
 import Icon_login from '../icons/login';
 import Icon_logout from '../icons/logout';
+import Icon_doc from '../icons/doc';
+
+// UTILS
+import UTILS from '../../utils/index';
 
 // STYLES
 import style from './style.module.css';
@@ -58,8 +62,11 @@ class Sidebar extends React.Component {
       >
         <div className={cn(style['sidebarctr-sidebar'])}>
           <div className={cn(style['sidebarctr-sidebar-logo'])}>
-            <img src="/images/safubase.png" />
+            <a href="https://safubase.com" target="_blank">
+              <img src="/images/safubase.png" />
+            </a>
           </div>
+
           <div className={cn(style['sidebarctr-sidebar-top'])}>
             <a className={cn(style['sidebarctr-sidebar-top-iconctr'])} href="/">
               <Icon_home active={this.state.pathname === '/' ? true : false} />
@@ -73,32 +80,19 @@ class Sidebar extends React.Component {
                 Home
               </span>
             </a>
+          </div>
 
+          <div className={cn(style['sidebarctr-sidebar-top'])}>
             <a
               className={cn(style['sidebarctr-sidebar-top-iconctr'])}
-              href="/profile"
+              href="https://docs.safubase.com"
             >
-              <Icon_profile
-                active={this.state.pathname === '/profile' ? true : false}
+              <Icon_doc
+                active={
+                  this.state.pathname === 'docs.safubase.com' ? true : false
+                }
               />
-              <span
-                className={cn(
-                  this.context.state.ui_sidebar_open
-                    ? style['sidebarctr-sidebar-top-iconctr-spanactive']
-                    : null
-                )}
-              >
-                Profile
-              </span>
-            </a>
 
-            <a
-              className={cn(style['sidebarctr-sidebar-top-iconctr'])}
-              href="/settings"
-            >
-              <Icon_settings
-                active={this.state.pathname === '/settings' ? true : false}
-              />
               <span
                 className={cn(
                   this.context.state.ui_sidebar_open
@@ -106,26 +100,42 @@ class Sidebar extends React.Component {
                     : null
                 )}
               >
-                Settings
-              </span>
-            </a>
-
-            <a className={cn(style['sidebarctr-sidebar-top-iconctr'])} href="#">
-              <Icon_home />
-              <span
-                className={cn(
-                  this.context.state.ui_sidebar_open
-                    ? style['sidebarctr-sidebar-top-iconctr-spanactive']
-                    : null
-                )}
-              >
-                Home
+                DOCS
               </span>
             </a>
           </div>
+
           <div className={cn(style['sidebarctr-sidebar-bottom'])}>
-            <div className={cn(style['sidebarctr-sidebar-bottom-iconctr'])}>
-              {this.context.state.auth ? (
+            <div
+              onClick={async () => {
+                if (!this.context.state.wallet_address) {
+                  const accounts = await UTILS.wallet_connect({ chain_id: 56 });
+
+                  if (accounts === null) {
+                    this.context.set_state({
+                      ...this.context.state,
+                      ui_toasts: [
+                        ...this.context.state.ui_toasts,
+                        {
+                          type: 'error',
+                          message: 'No web3 wallet detected in the browser',
+                          created_at: new Date(),
+                        },
+                      ],
+                    });
+
+                    return;
+                  }
+
+                  this.context.set_state({
+                    ...this.context.state,
+                    wallet_address: accounts[0],
+                  });
+                }
+              }}
+              className={cn(style['sidebarctr-sidebar-bottom-iconctr'])}
+            >
+              {this.context.state.wallet_address ? (
                 <>
                   <Icon_logout />
                   <span
