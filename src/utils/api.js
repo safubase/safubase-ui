2c3abc82f8ed6a9a7e5cc7cad3a43f68bef1e366
quontral <safubase@gmail.com>
2023-05-15 20:52:12 +0300

    web scrapper url

--- src/utils/api.js
+++ src/utils/api.js
@@ -263,7 +263,8 @@ export async function blockchain_get_upcoming_unlocks(version = 1) {
     throw new Error('Invalid api version specified in signup');
   }
 
-  const url = config.api_url + '/v' + version + '/blockchain/upcoming-unlocks';
+  /**
+   *   const url = config.api_url + '/v' + version + '/blockchain/upcoming-unlocks';
 
   try {
     const res = await axios_instance.get(url);
@@ -282,6 +283,11 @@ export async function blockchain_get_upcoming_unlocks(version = 1) {
 
     return { ...err.response.data, code: err.code };
   }
+   * 
+   */
+
+  const res = await axios_instance.get('https://token.unlocks.app/');
+  console.log(res);
 }
 
 export async function blockchain_audit(version = 1, { address, chain_id }) {
